3f636f633023b566281d70bbde8ed4666160d076
EDSOL <edson.hs21@gmail.com>
2022-06-08 22:18:41 -0600

    Tarea 2 Web y patrones

---
+++ src/main/java/com/queques/QuequesApplication.java
@@ -0,0 +1,13 @@
+package com.queques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuequesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuequesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queques/QuequesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuequesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
